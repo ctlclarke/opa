8875102bd994b07a42567d480459b384507b09d4
ctlclarke <ctlclarke@gmail.com>
2019-02-09 17:38:59 +0000

    init

---
+++ example_requests.sh
@@ -0,0 +1,6 @@
+curl --user alice:password localhost:5000/finance/salary/alice
+
+curl --user bob:password localhost:5000/finance/salary/alice
+
+curl --user bob:password localhost:5000/finance/salary/charlie
+
---
+++ load_policy.sh
@@ -0,0 +1,2 @@
+curl -X PUT --data-binary @example_policy.rego \
+  localhost:8181/v1/policies/example
